Fix Image scaling problem, re-scale images for 512,512 to 256,256

Common/MedicalDataLoading.py
@@ -31,9 +31,14 @@ def MedicalDataLoading(path):
         image = np.expand_dims(cv2.imread(myFile, cv2.IMREAD_GRAYSCALE), axis=0).astype('float32') #Grayscale loading but dim #1 save to be compatible with Unet loading
         if image.shape == (1,256,256):
             X_data.append(image)
+        elif image.shape == (1,512,512):
+            dst = cv2.pyrDown(image[0,:,:],dstsize=(256,256))
+            dst = np.expand_dims(dst,0)
+            X_data.append(dst)
+            logging.info('File {} Convert to (256,256)'.format(myFile))
         else:
-            logging.info('File {} not added to Dataloader'.format(myFile))
-            logging.info('Image size might be different that (256,256). The size actuarl image size %s', image.shape)
+            logging.debug('File {} not added to Dataloader'.format(myFile))
+            logging.debug('Image size might be different that (256,256). The size actuarl image size %s', image.shape)
     print('X_data shape:', np.array(X_data).shape)
 
     Y_data = []
@@ -43,9 +48,15 @@ def MedicalDataLoading(path):
         image = np.expand_dims(cv2.imread(myFile, cv2.IMREAD_GRAYSCALE), axis=0).astype('float32') #Grayscale loading but dim #1 save to be compatible with Unet loading
         if image.shape == (1,256,256):
             Y_data.append(image)
+        elif image.shape == (1,512,512):
+            dst = cv2.pyrDown(image[0,:,:],dstsize=(256,256))
+            dst = np.expand_dims(dst,0)
+            Y_data.append(dst)
+            logging.info('File {} Convert to (256,256)'.format(myFile))
         else:
-            logging.info('File {} not added to Dataloader'.format(myFile))
-            logging.info('Image size might be different that (256,256). The size actuarl image size %s', image.shape)
+            logging.debug('File {} not added to Dataloader'.format(myFile))
+            logging.debug('Image size might be different that (256,256). The size actuarl image size %s', image.shape)
+
     print('Y_data shape:', np.array(Y_data).shape)
 
     X_train, X_test, y_train, y_test = train_test_split(X_data, Y_data, test_size=0.2, random_state=1)