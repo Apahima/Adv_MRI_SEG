45951b114b3860bc96b9afc4a525b31559bb8d04
Apahima <fahimaadi@gmail.com>
2020-06-19 10:45:53 +0300

    Unified the parser for Model, Remove parser function block from Main_Model.py and add to Unet_Medical_Parser

--- Common/Unet_Medical_Parse.py
+++ Common/Unet_Medical_Parse.py
@@ -38,3 +38,44 @@ class Args(argparse.ArgumentParser):
 
         # Override defaults with passed overrides
         self.set_defaults(**overrides)
+
+
+def create_arg_parser():
+    parser = Args()
+    # model parameters
+    parser.add_argument('--num-pools', type=int, default=3, help='Number of U-Net pooling layers')
+    parser.add_argument('--drop-prob', type=float, default=0.0, help='Dropout probability')
+    parser.add_argument('--num-chans', type=int, default=2, help='Number of U-Net channels')
+
+    parser.add_argument('--batch-size', default=16, type=int, help='Mini batch size')
+    parser.add_argument('--num-epochs', type=int, default=5000, help='Number of training epochs')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--lr-step-size', type=int, default=40,
+                        help='Period of learning rate decay')
+    parser.add_argument('--lr-gamma', type=float, default=0.1,
+                        help='Multiplicative factor of learning rate decay')
+    parser.add_argument('--weight-decay', type=float, default=0.,
+                        help='Strength of weight decay regularization')
+    parser.add_argument('--momentum', type=float, default=0.,
+                        help='Momentum factor')
+
+    parser.add_argument('--report-interval', type=int, default=100, help='Period of loss reporting')
+    parser.add_argument('--data-parallel', action='store_true',
+                        help='If set, use multiple GPUs using data parallelism')
+    parser.add_argument('--device', type=str, default='cuda',
+                        help='Which device to train on. Set to "cuda" to use the GPU')
+    parser.add_argument('--exp-dir', type=pathlib.Path, default='checkpoints',
+                        help='Path where model and results should be saved')
+    parser.add_argument('--resume', action='store_true',
+                        help='If set, resume the training from a previous model checkpoint. '
+                             '"--checkpoint" should be set with this')
+    parser.add_argument('--checkpoint', type=str,
+                        help='Path to an existing checkpoint. Used along with "--resume"')
+    parser.add_argument('--output-chans',type=int, default=1,
+                        help='For multi-segmentation used Chans -> #Classes for single segmentation used Chans -> 1')
+
+
+    # parser.add_argument('--data-split', choices=['val', 'test'], required=True,
+    #                     help='Which data partition to run on: "val" or "test"')
+    # This feature is dedicate for supervised learning and H5 files
+    return parser
