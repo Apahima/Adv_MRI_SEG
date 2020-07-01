2f8de0a018281552128732c46d93b1e421027165
Apahima <fahimaadi@gmail.com>
2020-07-02 00:44:29 +0300

    bash script

--- TrainingList.sh
+++ TrainingList.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-python Main_Model.py --challenge singlecoil --num-chans 32 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 32 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --lr 0.0001 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 32 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --lr 0.05 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --lr 0.0001 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --lr 0.05 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 32 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 3 --batch-size 20 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 32 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 3 --batch-size 20 --lr 0.0001 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 32 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 3 --batch-size 20 --lr 0.05 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 3 --batch-size 20 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 3 --batch-size 20 --lr 0.0001 --num-epoch 100
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 3 --batch-size 20 --lr 0.05 --num-epoch 100
+python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --num-epoch 50 --loss Tversky --alpha-tversky 0.1 --beta-tversky 0.9
+
