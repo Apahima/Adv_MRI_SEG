49b01a4da93e682ebf9fd105c498d61f84a00a71
Apahima <fahimaadi@gmail.com>
2020-07-02 00:45:06 +0300

    bash script

--- TrainingList.sh
+++ TrainingList.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --num-epoch 50 --loss Tversky --alpha-tversky 0.1 --beta-tversky 0.9
+python Main_Model.py --challenge singlecoil --num-chans 64 --drop-prob 0.5 --data-path Data/ISPY1  --num-pools 5 --batch-size 20 --exp-dir 28062020 --num-epoch 50 --loss Tversky --alpha-tversky 0.1 --beta-tversky 0.9
 
